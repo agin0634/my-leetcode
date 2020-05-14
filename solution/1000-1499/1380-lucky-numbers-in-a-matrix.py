cfc1a4b658c791e7e0386b4c13f3999cb2f0b326
Duri Shen <agin0511@gmail.com>
2020-05-14 11:20:07 +0800

    update 1380

--- solution/1000-1499/1380-lucky-numbers-in-a-matrix.py
+++ solution/1000-1499/1380-lucky-numbers-in-a-matrix.py
@@ -4,16 +4,6 @@ class Solution(object):
         :type matrix: List[List[int]]
         :rtype: List[int]
         """
-        max_in_col, min_in_row = [], []
-        res = []
-        for i in range(len(matrix[0])):
-            max = float('-inf')
-            for j in range(len(matrix)):
-                if matrix[j][i] > max:
-                    max = matrix[j][i]
-            max_in_col.append(max)
-        for m in matrix:
-            n = min(m)
-            if n in max_in_col:
-                res.append(n)
-        return res
+        min_row = {min(i) for i in matrix}
+        max_col = {max(j) for j in zip(*matrix)}
+        return list(max_col & min_row)
