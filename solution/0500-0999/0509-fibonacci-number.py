958af203850fd32ca5b6e3e80de3aede4a2b8f36
Duri Shen <agin0511@gmail.com>
2020-05-21 15:26:10 +0800

    update 509

--- solution/0500-0999/0509-fibonacci-number.py
+++ solution/0500-0999/0509-fibonacci-number.py
@@ -1,8 +1,10 @@
 class Solution(object):
+    table = {0:0, 1:1}
     def fib(self, N):
         """
         :type N: int
         :rtype: int
         """
-        if N <= 1: return N
-        return self.fib(N-1) + self.fib(N-2)
+        if N in self.table: return self.table[N]
+        self.table[N] = self.fib(N-1) + self.fib(N-2)
+        return self.table[N]
