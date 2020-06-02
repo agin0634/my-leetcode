15329a53047e55b6bd8405a368a87cc3ff27b88a
Duri Shen <agin0511@gmail.com>
2020-06-02 14:36:50 +0800

    update 268

--- solution-python/0000-0499/0268-missing-number.py
+++ solution-python/0000-0499/0268-missing-number.py
@@ -4,6 +4,12 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        for i in range(len(nums)+1):
-            if i not in nums:
-                return i
+        nums = sorted(nums)
+        if nums[-1] != len(nums):
+            return len(nums)
+        elif nums[0] != 0:
+            return 0
+        for i in range(1,len(nums)):
+            n = nums[i-1]+1
+            if nums[i] != n:
+                return n
